2ae7a03fb1f41c211149f96e380e61ae00b3a011
McGrath <jennifer.mcgrath@sportsengine.com>
2025-06-02 12:28:16 -0600

    PUT events/:id test

--- spec/requests/event_spec.rb
+++ spec/requests/event_spec.rb
@@ -9,7 +9,7 @@ describe EventsController do
 
     describe "GET /events" do
         it "returns a list of events" do
-            get "/events" #events_path # Assuming you have a route defined for events
+            get events_path # Assuming you have a route defined for events
             expect(response).to have_http_status(:ok)
             expect(JSON.parse(response.body).size).to be >= 0
         end
@@ -19,22 +19,29 @@ describe EventsController do
         it "creates a new event" do
             user = User.first
             sign_in user
-            post "/events", params: { event: { creator: user.id, title: "Test Event", time: "2023-10-01", location: "test location", description: "fun" } }
+            post events_path, params: { event: { creator: user.id, title: "Test Event", time: "2023-10-01", location: "test location", description: "fun" } }
             expect(response).to have_http_status(:found)
             expect(response).to redirect_to(events_path)
             expect(Event.last.title).to eq("Test Event")
         end
     end
     
-    # describe "PUT /events/:id" do
-    #     let!(:event) { create(:event, name: "Old Event") }
-    
-    #     it "updates an existing event" do
-    #     put event_path(event), params: { event: { name: "Updated Event" } }
-    #     expect(response).to have_http_status(:ok)
-    #     expect(JSON.parse(response.body)["name"]).to eq("Updated Event")
-    #     end
-    # end
+    describe "PUT /events/:id" do
+        user = FactoryBot.create(:user)
+        event = FactoryBot.create(:event, creator: user, title: "Original Title")
+        # let!(:event) { create(:event, title: "Old Event") }
+        
+        it "updates an existing event" do
+            # user = User.find(event.user_id)
+            sign_in user
+            patch "/events/#{event.id}", params: { event: { title: "Updated Title" } } 
+            puts "Event === #{event.inspect}"
+            expect(response).to have_http_status(:found)
+            expect(response).to redirect_to(event_path(event))
+            event.reload
+            expect(event.title).to eq("Updated Title")
+        end
+    end
     
     # describe "DELETE /events/:id" do
     #     let!(:event) { create(:event) }
