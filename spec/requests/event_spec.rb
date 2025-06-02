8a6cb6921008312718a700830b79bc327e4e3825
McGrath <jennifer.mcgrath@sportsengine.com>
2025-06-02 14:58:27 -0600

    unauthorized user and /events

--- spec/requests/event_spec.rb
+++ spec/requests/event_spec.rb
@@ -73,4 +73,50 @@ describe "Events API", type: :request do
             end
         end
     end
+    describe "as an unauthorized user" do
+
+        describe "GET /events" do
+            # before { FactoryBot.create_list(:event, 5) }
+            it "returns a list of events" do
+                get events_path
+                expect(response).to have_http_status(:ok)
+                expect(JSON.parse(response.body).size).to be >= 5
+            end
+        end
+        
+        describe "POST /events" do
+            it "redirects to user login page" do
+                post events_path, params: { event: { title: "Unauthorized Event", time: "2023-10-01", location: "test location", description: "fun" } }
+                expect(response).to have_http_status(:found)
+                expect(response).to redirect_to(new_user_session_path) #  Redirect to login 
+            end
+        end
+        
+        describe "PUT /events/:id" do
+            let(:event) { FactoryBot.create(:event) }
+            it "redirects to user login page" do
+                patch event_path(event), params: { event: { title: "Updated Title" } }
+                expect(response).to have_http_status(:found)
+                expect(response).to redirect_to(new_user_session_path) #  Redirect to login 
+            end
+        end
+        
+        describe "DELETE /events/:id" do
+            let(:event) { FactoryBot.create(:event) }
+            it "redirects to user login page" do
+                delete event_path(event)
+                expect(response).to have_http_status(:found)
+                expect(response).to redirect_to(new_user_session_path) #  Redirect to login 
+            end
+        end
+
+        describe "POST /events/:id/invite" do
+            let(:event) { FactoryBot.create(:event) }
+            it "redirects to user login page" do
+                post invite_event_path(event), params: { user_ids: [1, 2] }
+                expect(response).to have_http_status(:found)
+                expect(response).to redirect_to(new_user_session_path) #  Redirect to login 
+            end
+        end
+    end
 end
